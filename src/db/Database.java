3901091ceba50a99911a79431d4b395a12c01fb3
Mobin <Mobinmojiri1234@gmail.com>
2025-03-21 20:04:19 +0330

    creating methods of Database

--- src/db/Database.java
+++ src/db/Database.java
@@ -1,4 +1,50 @@
 package db;
+import dbexeption.EntityNotFoundException;
+import java.util.ArrayList;
+
 
 public class Database {
+
+
+    private static ArrayList<Entity> entities = new ArrayList<>();
+
+    static void add(Entity e){
+        entities.add(e);
+    }
+
+
+    static Entity get(int id) throws EntityNotFoundException {
+        for(Entity entity: entities){
+            if(entity.id == id){
+                return entity;
+            }
+        }
+        throw new EntityNotFoundException();
+    }
+
+
+    static void delete(int id) throws EntityNotFoundException{
+        for(Entity entity: entities){
+            if(entity.id == id){
+                entities.remove(entity);
+                return;
+            }
+        }
+        throw new EntityNotFoundException();
+    }
+
+
+    static void update(Entity e)throws EntityNotFoundException{
+        int i = 0;
+        for(Entity entity: entities){
+            if(entity.id == e.id){
+                entities.set(i, e);
+                return;
+            }
+            i++;
+        }
+        throw new EntityNotFoundException();
+    }
+
+
 }
