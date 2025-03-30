6852f6373390451e49975f539efaa5dedc39a2bb
Mobin <Mobinmojiri1234@gmail.com>
2025-03-30 15:27:28 +0330

    check test of cloning

--- src/example/Human.java
+++ src/example/Human.java
@@ -1,18 +1,16 @@
 package example;
 import db.Entity;
 
-public class Human extends Entity {
+public class Human extends Entity{
     public String name;
 
     public Human(String name) {
         this.name = name;
     }
 
-    @Override
-    public Human copy() {
-        Human copyHuman = new Human(name);
-        copyHuman.id = id;
 
-        return copyHuman;
+    @Override
+    public Human clone() {
+        return (Human) super.clone();
     }
 }
