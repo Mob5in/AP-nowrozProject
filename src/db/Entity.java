c618d48f335b2b658c54c981c21d6aa19803a344
Mobin <Mobinmojiri1234@gmail.com>
2025-03-30 15:22:09 +0330

    implements Cloneable and clone() to Entity

--- src/db/Entity.java
+++ src/db/Entity.java
@@ -1,12 +1,18 @@
 package db;
 
-public abstract class Entity {
+public abstract class Entity implements Cloneable{
 
     public int id = 0;
     public Entity(){
         id ++;
     }
 
-    public abstract Entity copy();
-
+    @Override
+    public Entity clone() {
+        try {
+            return (Entity) super.clone();
+        } catch (CloneNotSupportedException e) {
+            throw new AssertionError("Clone not supported");
+        }
+    }
 }
