1c895fa15d77b79e207a25e063323c2128506fc4
Mobin <Mobinmojiri1234@gmail.com>
2025-04-01 12:36:57 +0330

    code registerValidators

--- src/db/Database.java
+++ src/db/Database.java
@@ -6,12 +6,22 @@ import java.util.HashMap;
 
 public class Database {
 
-    private static HashMap<Integer, Validator> validators;
+    private static HashMap<Integer, Validator> validators = new HashMap<>();
     private static ArrayList<Entity> entities = new ArrayList<>();
 
     public static void add(Entity e){
         e.id = entities.size() + 1;
         entities.add(e.clone());
+    }
+
+
+    public void getValidators(){
+
+        System.out.println("Validators code:");
+
+        for (int i : validators.keySet()) {
+            System.out.println(i);
+        }
     }
 
 
@@ -48,6 +58,13 @@ public class Database {
         throw new EntityNotFoundException();
     }
 
+    public static void registerValidator(int entityCode, Validator validator) {
 
+        if(validators.get(entityCode) != null){
+            throw new IllegalArgumentException("the entity code already exist");
+        }
+
+        validators.put(entityCode, validator);
+    }
 
 }
