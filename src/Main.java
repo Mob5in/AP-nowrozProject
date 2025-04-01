50bddc51c0e26320ca9a095aead88466bf417895
Mobin <Mobinmojiri1234@gmail.com>
2025-04-01 12:45:21 +0330

    testing code and it was successful

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,11 @@ import dbexeption.*;
 import example.*;
 
 public class Main {
+        public static void main(String[] args) throws InvalidEntityException {
+            Database.registerValidator(Human.HUMAN_ENTITY_CODE, new HumanValidator());
 
+            Human ali = new Human("Ali", -10);
+            Database.add(ali);
+        }
 
 }
