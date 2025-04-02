ba3a49937ea73c1256ae39e3044099e4e7336b3e
Mobin <Mobinmojiri1234@gmail.com>
2025-04-02 12:59:05 +0330

    check test code for this part

--- src/Main.java
+++ src/Main.java
@@ -12,24 +12,24 @@ public class Main {
 
         System.out.println("id: " + doc.id);
         System.out.println("content: " + doc.content);
-        System.out.println("creation date: " + doc.getCreationDate());
-        System.out.println("last modification date: " + doc.getLastModificationDate());
-        System.out.println();
-
-        try {
-            Thread.sleep(30_000);
-        } catch (InterruptedException e) {
-            System.out.println("Sleep interrupted!");
-        }
-
-        doc.content = "This is the new content";
-
-        Database.update(doc);
-
-        System.out.println("Document updated");
-        System.out.println("id: " + doc.id);
-        System.out.println("content: " + doc.content);
-        System.out.println("creation date: " + doc.getCreationDate());
-        System.out.println("last modification date: " + doc.getLastModificationDate());
+//        System.out.println("creation date: " + doc.getCreationDate());
+//        System.out.println("last modification date: " + doc.getLastModificationDate());
+//        System.out.println();
+//
+//        try {
+//            Thread.sleep(30_000);
+//        } catch (InterruptedException e) {
+//            System.out.println("Sleep interrupted!");
+//        }
+//
+//        doc.content = "This is the new content";
+//
+//        Database.update(doc);
+//
+//        System.out.println("Document updated");
+//        System.out.println("id: " + doc.id);
+//        System.out.println("content: " + doc.content);
+//        System.out.println("creation date: " + doc.getCreationDate());
+//        System.out.println("last modification date: " + doc.getLastModificationDate());
     }
 }
