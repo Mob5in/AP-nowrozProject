12a976d24e844cba9fceb968b3f8aebf76ac9493
Mobin <Mobinmojiri1234@gmail.com>
2025-04-02 11:01:53 +0330

    add superclass and implements

--- src/example/Document.java
+++ src/example/Document.java
@@ -1,4 +1,34 @@
 package example;
+import db.Entity;
+import db.Trackable;
+import java.util.Date;
 
-public class Document {
+
+public class Document extends Entity implements Trackable {
+
+
+    @Override
+    public int getEntityCode() {
+        return 0;
+    }
+
+    @Override
+    public void setCreationDate(Date date) {
+
+    }
+
+    @Override
+    public Date getCreationDate() {
+        return null;
+    }
+
+    @Override
+    public void setLastModificationDate(Date date) {
+
+    }
+
+    @Override
+    public Date getLastModificationDate() {
+        return null;
+    }
 }
