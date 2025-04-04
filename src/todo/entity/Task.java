be6625cc6762ab6c77716d710158f1c963991a64
Mobin <Mobinmojiri1234@gmail.com>
2025-04-04 15:14:26 +0330

    coding fundmentals of Task

--- src/todo/entity/Task.java
+++ src/todo/entity/Task.java
@@ -1,4 +1,51 @@
 package todo.entity;
+import db.Entity;
+import db.Trackable;
+import java.util.Date;
 
-public class Task {
+public class Task extends Entity implements Trackable {
+
+
+
+    enum Status {
+        NotStarted,
+        InProgress,
+        Completed
+    }
+
+    private String title;
+    private String description;
+    private Date dueDate;
+    private Status status;
+
+
+
+    @Override
+    public int getEntityCode() {
+        return 0;
+    }
+
+
+    @Override
+    public void setCreationDate(Date date) {
+
+    }
+
+    @Override
+    public Date getCreationDate() {
+        return null;
+    }
+
+
+    @Override
+    public void setLastModificationDate(Date date) {
+
+    }
+
+    @Override
+    public Date getLastModificationDate() {
+        return null;
+    }
+
+
 }
