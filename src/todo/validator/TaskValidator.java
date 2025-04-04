e977ecc010d3d482124159aef99e15fab6615c0b
Mobin <Mobinmojiri1234@gmail.com>
2025-04-04 16:27:26 +0330

    coding structure of TaskValidator

--- src/todo/validator/TaskValidator.java
+++ src/todo/validator/TaskValidator.java
@@ -2,13 +2,20 @@ package todo.validator;
 import db.Entity;
 import db.Validator;
 import dbexeption.InvalidEntityException;
+import todo.entity.Task;
 
 
 public class TaskValidator implements Validator {
 
     @Override
     public void validate(Entity entity) throws InvalidEntityException {
-
-
+        if(!(entity instanceof Task)){
+            throw new IllegalArgumentException("Entity must be task");
+        }
+        if(((Task) entity).getTitle().isEmpty()){
+            throw new IllegalArgumentException("Title must not be empty");
+        }
     }
+
+
 }
