9c57c340de1e4053fcb0e964b664be399e8a234d
Mobin <Mobinmojiri1234@gmail.com>
2025-04-04 16:16:01 +0330

    coding structure of Task

--- src/todo/entity/Task.java
+++ src/todo/entity/Task.java
@@ -13,38 +13,48 @@ public class Task extends Entity implements Trackable {
         Completed
     }
 
+    static final int TASK_ENTITY_CODE = 4;
     private String title;
     private String description;
     private Date dueDate;
     private Status status;
+    private Date creationDate;
+    private Date lastModificationDate;
 
-
+    public Task(String title, String description, Date dueDate){
+        this.title = title;
+        this.description = description;
+        this.dueDate = dueDate;
+        this.status = Status.NotStarted;
+        setCreationDate(new Date());
+        setLastModificationDate(new Date());
+    }
 
     @Override
     public int getEntityCode() {
-        return 0;
+        return TASK_ENTITY_CODE;
     }
 
 
     @Override
     public void setCreationDate(Date date) {
-
+        this.creationDate = date;
     }
 
     @Override
     public Date getCreationDate() {
-        return null;
+        return creationDate;
     }
 
 
     @Override
     public void setLastModificationDate(Date date) {
-
+        this.lastModificationDate = date;
     }
 
     @Override
     public Date getLastModificationDate() {
-        return null;
+        return lastModificationDate;
     }
 
 
