0947de209324c719187e09d9139af5826ea2f74a
Mobin <Mobinmojiri1234@gmail.com>
2025-04-04 16:20:09 +0330

    coding structure of STEP

--- src/todo/entity/Step.java
+++ src/todo/entity/Step.java
@@ -4,10 +4,7 @@ import db.Entity;
 
 public class Step extends Entity {
 
-    @Override
-    public int getEntityCode() {
-        return 0;
-    }
+
 
     enum Status{
         NotStarted,
@@ -17,6 +14,16 @@ public class Step extends Entity {
     private String title;
     private Status status;
     private int taskRef;
+    static final int STEP_ENTITY_CODE = 12;
 
+    @Override
+    public int getEntityCode() {
+        return STEP_ENTITY_CODE;
+    }
 
+    public Step(String title, Task task){
+        this.title = title;
+        this.status = Status.NotStarted;
+        this.taskRef = task.id;
+    }
 }
