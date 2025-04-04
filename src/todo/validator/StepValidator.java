71547bf5e1035d28b9088e1dd782f74bda679734
Mobin <Mobinmojiri1234@gmail.com>
2025-04-04 15:47:41 +0330

    coding fundmentals of StepValidator

--- src/todo/validator/StepValidator.java
+++ src/todo/validator/StepValidator.java
@@ -1,4 +1,11 @@
 package todo.validator;
+import db.Entity;
+import db.Validator;
+import dbexeption.InvalidEntityException;
 
-public class StepValidator {
+public class StepValidator implements Validator {
+    @Override
+    public void validate(Entity entity) throws InvalidEntityException {
+
+    }
 }
