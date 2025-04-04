d188ec0bfa01945d8c18e8d9164048898607747e
Mobin <Mobinmojiri1234@gmail.com>
2025-04-04 16:56:03 +0330

    adding update database in TaskService

--- src/todo/service/TaskService.java
+++ src/todo/service/TaskService.java
@@ -1,20 +1,23 @@
 package todo.service;
 
 import db.Database;
+import dbexeption.InvalidEntityException;
 import todo.entity.Task;
 
 
 public class TaskService {
 
 
-    public static void setAsCompleted(int taskId) {
+    public static void setAsCompleted(int taskId) throws InvalidEntityException {
         Task task = (Task) Database.get(taskId);
         task.status = Task.Status.Completed;
+        Database.update(task);
     }
 
-    public static void setAsInProgress(int taskId) {
+    public static void setAsInProgress(int taskId) throws InvalidEntityException {
         Task task = (Task) Database.get(taskId);
         task.status = Task.Status.InProgress;
+        Database.update(task);
     }
 
 }
