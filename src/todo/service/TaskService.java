220b2ccb5a0245b21a1c3d72a53da129d3bc12a8
Mobin <Mobinmojiri1234@gmail.com>
2025-04-04 16:43:01 +0330

    coding structure of TaskService

--- src/todo/service/TaskService.java
+++ src/todo/service/TaskService.java
@@ -1,14 +1,20 @@
 package todo.service;
 
+import db.Database;
+import todo.entity.Task;
+
+
 public class TaskService {
 
 
     public static void setAsCompleted(int taskId) {
-
+        Task task = (Task) Database.get(taskId);
+        task.status = Task.Status.Completed;
     }
 
     public static void setAsInProgress(int taskId) {
-
+        Task task = (Task) Database.get(taskId);
+        task.status = Task.Status.InProgress;
     }
 
 }
