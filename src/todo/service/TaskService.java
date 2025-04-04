acc465229e853bb8b64b6cd257ce9aa9d5f1bea8
Mobin <Mobinmojiri1234@gmail.com>
2025-04-04 15:50:00 +0330

    coding fundmentals of TaskService

--- src/todo/service/TaskService.java
+++ src/todo/service/TaskService.java
@@ -1,4 +1,14 @@
 package todo.service;
 
 public class TaskService {
+
+
+    public static void setAsCompleted(int taskId) {
+
+    }
+
+    public static void setAsInProgress(int taskId) {
+
+    }
+
 }
