cffad2e898fdb57de643e2ceacfccaec59ce489b
Mobin <Mobinmojiri1234@gmail.com>
2025-04-04 15:40:19 +0330

    coding fundmentals of step

--- src/todo/validator/TaskValidator.java
+++ src/todo/validator/TaskValidator.java
@@ -1,4 +1,14 @@
 package todo.validator;
+import db.Entity;
+import db.Validator;
+import dbexeption.InvalidEntityException;
 
-public class TaskValidator {
+
+public class TaskValidator implements Validator {
+
+    @Override
+    public void validate(Entity entity) throws InvalidEntityException {
+
+
+    }
 }
