5e36d1bb829b3810d1560cf3a797284e3624f57d
Mobin <Mobinmojiri1234@gmail.com>
2025-04-04 17:00:28 +0330

    name method of this class for then to complete them

--- src/todo/service/StepService.java
+++ src/todo/service/StepService.java
@@ -1,8 +1,17 @@
 package todo.service;
 
+import db.Database;
+import dbexeption.InvalidEntityException;
+import todo.entity.Step;
+
 public class StepService {
 
-    public static void saveStep(int taskRef, String title) {
+    public static void saveStep(Step step) throws InvalidEntityException {
+        Database.add(step);
+    }
+
+    public static void setAsComplete(){
 
     }
+
 }
